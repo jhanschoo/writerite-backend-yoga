f43a2cddffbaf241ad2a62303b44d57d9ecb21e6
Johannes Choo <jhanschoo@gmail.com>
2018-10-09 15:56:53 +0800

    add field resolvers to index

--- src/resolver/index.ts
+++ src/resolver/index.ts
@@ -1,13 +1,21 @@
 import Query from './Query';
 import Mutation from './Mutation';
 import Subscription from './Subscription';
+import { Card } from './Card';
 import { Deck } from './Deck';
+import { Room } from './Room';
+import { RoomMessage } from './RoomMessage';
+import { User } from './User';
 
 const resolvers: any = {
   Query,
   Mutation,
   Subscription,
+  Card,
   Deck,
+  Room,
+  RoomMessage,
+  User,
 };
 
 export default resolvers;
