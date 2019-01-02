d742c499dd19ac53f7526e8b475e10758ba20997
Johannes Choo <jhanschoo@gmail.com>
2019-01-03 02:45:56 +0800

    add room message query import

--- src/resolver/Query/index.ts
+++ src/resolver/Query/index.ts
@@ -2,12 +2,14 @@ import { rwCardQuery } from './RwCard';
 import { rwDeckQuery } from './RwDeck';
 import { rwRoomQuery } from './RwRoom';
 import { rwUserQuery } from './RwUser';
+import { rwRoomMessageQuery } from './RwRoomMessage';
 
 // tslint:disable-next-line: variable-name
 const Query = {
   ...rwCardQuery,
   ...rwDeckQuery,
   ...rwRoomQuery,
+  ...rwRoomMessageQuery,
   ...rwUserQuery,
 };
 
