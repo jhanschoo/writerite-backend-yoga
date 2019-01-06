7e946fe7b58932cb8fe3dfaf7ea7e2c91ae15382
Johannes Choo <jhanschoo@gmail.com>
2019-01-06 17:44:03 +0800

    use OR properly in prisma client to reduce prisma call in RwRoomMessage.mutation

--- src/resolver/Mutation/RwRoomMessage.mutation.ts
+++ src/resolver/Mutation/RwRoomMessage.mutation.ts
@@ -19,13 +19,18 @@ const rwRoomMessageCreate: IFieldResolver<any, IRwContext, {
       throw wrAuthenticationError();
     }
     const isAcolyte = sub.roles.includes(Roles.acolyte);
-    if (!isAcolyte && await prisma.pRoom({ id: roomId }).owner().id() !== sub.id) {
-      // workaround for OR not working properly in prisma client
-      // c.f. https://github.com/prisma/prisma/issues/3763
-      if (!(await prisma.pRoom({ id: roomId }).occupants() || [])
-        .map((user) => user.id).includes(sub.id)) {
-        throw wrNotFoundError('room');
-      }
+    if (!isAcolyte && (await prisma.pRooms({
+        where: {
+          id: roomId,
+          OR: [{
+            occupants_some: { id: sub.id },
+          }, {
+            owner: { id: sub.id },
+          }],
+        },
+      }) || []).length !== 1
+    ) {
+      throw wrNotFoundError('room');
     }
     const pRoomMessage = await prisma.createPRoomMessage({
       content,
